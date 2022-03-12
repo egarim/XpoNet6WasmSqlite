a9a1542303b08b5057b688d7c18b4ac9e0c83f26
Jose Manuel Ojeda Melgar <joche.ojeda@bitframeworks.com>
2022-03-12 12:16:01 -0700

    Xpo Webassembly

---
+++ XpoNet6WasmSqlite/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Xpo;
+using Microsoft.AspNetCore.Components;
+
+namespace XpoNet6WasmSqlite.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        private UnitOfWork UoW { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+        private void Save()
+        {
+            User user = new User(this.UoW);
+            user.Name = this.Name;
+            UoW.CommitChanges();
+            this.Users.Add(user);
+        }
+        
+        protected override async Task OnInitializedAsync()
+        {
+            Users = this.UoW.Query<User>().ToList();
+        }
+    }
+}
