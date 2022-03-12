0e22d6dea9e3aa7c207141fc95902c46926f6a6a
Jose Manuel Ojeda Melgar <joche.ojeda@bitframeworks.com>
2022-03-12 11:22:03 -0700

    Add project files.

---
+++ XpoNet6WasmSqlite/Program.cs
@@ -0,0 +1,19 @@
+using DevExpress.Xpo.DB;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using XpoNet6WasmSqlite;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddXpoDefaultUnitOfWork(true, options =>
+            options
+                .UseConnectionString("XpoProvider=SQLite;Data Source=mydb.db")
+                // Remove this line if the database already exists.
+                .UseAutoCreationOption(AutoCreateOption.DatabaseAndSchema)
+                // Pass all of your persistent object types to this method.
+                .UseEntityTypes(new Type[] { typeof(User) })); 
+   
+await builder.Build().RunAsync();
---
+++ XpoNet6WasmSqlite/User.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+
+namespace XpoNet6WasmSqlite
+{
+    public class User:DevExpress.Xpo.XPObject
+    {
+        protected User()
+        {
+
+        }
+
+        public User(Session session) : base(session)
+        {
+
+        }
+
+        protected User(Session session, XPClassInfo classInfo) : base(session, classInfo)
+        {
+
+        }
+
+        string name;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+    }
+}
