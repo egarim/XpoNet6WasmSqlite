13af36de410874b846823f18a3d739e2b6babfb0
Jose Manuel Ojeda Melgar <joche.ojeda@bitframeworks.com>
2022-03-12 22:07:30 -0700

    Download file and keeping the state

--- XpoNet6WasmSqlite/Pages/Index.razor.cs
+++ XpoNet6WasmSqlite/Pages/Index.razor.cs
@@ -1,25 +1,89 @@
-﻿using DevExpress.Xpo;
+﻿using Blazored.LocalStorage;
+using DevExpress.Xpo;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Diagnostics;
 
 namespace XpoNet6WasmSqlite.Pages
 {
     public partial class Index
     {
+       
         [Inject]
+        private IJSRuntime js { get; set; }
+
+        [Inject]
+        private ILocalStorageService localStorage { get; set; }
+
+        //[Inject]
         private UnitOfWork UoW { get; set; }
         public string Name { get; set; }
-        public List<User> Users { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
         private void Save()
         {
+            var test=File.Exists("mydb.db");
+            Debug.WriteLine($"test{test}");
             User user = new User(this.UoW);
             user.Name = this.Name;
             UoW.CommitChanges();
             this.Users.Add(user);
+            this.Name = "";
+            SaveDatabaseToLocalStorage();
         }
-        
+        async void  DownloadFile()
+        {
+           
+            this.UoW.Disconnect();
+            var DbBytes=File.ReadAllBytes("mydb.db");
+            await FileUtil.SaveAs(js, "mydb.db", DbBytes);
+        }
+        async void SaveDatabaseToLocalStorage()
+        {
+            //this.UoW.Disconnect();
+            var DbBytes = File.ReadAllBytes("mydb.db");
+            await localStorage.SetItemAsync<byte[]>("mydb.db", DbBytes);
+        }
         protected override async Task OnInitializedAsync()
         {
+            var db= await localStorage.GetItemAsync<byte[]>("mydb.db");
+            if(db==null)
+            {
+                File.WriteAllBytes("mydb.db", GetResource("mydb.db"));
+            }
+            else
+            {
+                File.WriteAllBytes("mydb.db", db);
+            }
+            InitXpo();
             Users = this.UoW.Query<User>().ToList();
+        }
+        void InitXpo()
+        {
+            //best practice #7
+            //https://supportcenter.devexpress.com/ticket/details/a2944/xpo-best-practices
+            IDataLayer dl = XpoDefault.GetDataLayer("XpoProvider=SQLite;Data Source=mydb.db", DevExpress.Xpo.DB.AutoCreateOption.DatabaseAndSchema);
+            using (Session session = new Session(dl))
+            {
+                System.Reflection.Assembly[] assemblies = new System.Reflection.Assembly[] {
+                typeof(User).Assembly };
+       
+                session.UpdateSchema(assemblies);
+                session.CreateObjectTypeRecords(assemblies);
+            }
+            this.UoW=new UnitOfWork(dl);
+        }
+        public byte[] GetResource(string filename)
+        {
+            string result = string.Empty;
+
+            using (var stream = this.GetType().Assembly.
+                       GetManifestResourceStream("XpoNet6WasmSqlite." + filename))
+            {
+                var Ms = new MemoryStream();
+                stream.CopyTo(Ms);
+               return Ms.ToArray();
+            }
+           
         }
     }
 }
--- XpoNet6WasmSqlite/Program.cs
+++ XpoNet6WasmSqlite/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using DevExpress.Xpo.DB;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,12 +9,16 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddXpoDefaultUnitOfWork(true, options =>
-            options
-                .UseConnectionString("XpoProvider=SQLite;Data Source=mydb.db")
-                // Remove this line if the database already exists.
-                .UseAutoCreationOption(AutoCreateOption.DatabaseAndSchema)
-                // Pass all of your persistent object types to this method.
-                .UseEntityTypes(new Type[] { typeof(User) })); 
+builder.Services.AddBlazoredLocalStorage();
+
+
+
+//builder.Services.AddXpoDefaultUnitOfWork(true, options =>
+//            options
+//                .UseConnectionString("XpoProvider=SQLite;Data Source=mydb.db")
+//                // Remove this line if the database already exists.
+//                .UseAutoCreationOption(AutoCreateOption.DatabaseAndSchema)
+//                // Pass all of your persistent object types to this method.
+//                .UseEntityTypes(new Type[] { typeof(User) })); 
    
 await builder.Build().RunAsync();
