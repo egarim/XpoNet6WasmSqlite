b0f887d81e7b2699d83a978a65b43024fa096922
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-07-26 20:11:35 +0300

    Net7

--- XpoNet6WasmSqlite/Program.cs
+++ XpoNet6WasmSqlite/Program.cs
@@ -4,21 +4,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using XpoNet6WasmSqlite;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
 
 
-
-//builder.Services.AddXpoDefaultUnitOfWork(true, options =>
-//            options
-//                .UseConnectionString("XpoProvider=SQLite;Data Source=mydb.db")
-//                // Remove this line if the database already exists.
-//                .UseAutoCreationOption(AutoCreateOption.DatabaseAndSchema)
-//                // Pass all of your persistent object types to this method.
-//                .UseEntityTypes(new Type[] { typeof(User) })); 
-   
 await builder.Build().RunAsync();
